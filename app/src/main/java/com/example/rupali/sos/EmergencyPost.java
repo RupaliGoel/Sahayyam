496e5ee3caa82c982ac1a5dd34bc1566679c11fd
Ananta Singh <singhananta@gmail.com>
2018-10-03 12:39:10 +0530

    checking

--- app/src/main/java/com/example/rupali/sos/EmergencyPost.java
+++ app/src/main/java/com/example/rupali/sos/EmergencyPost.java
@@ -89,7 +89,6 @@ public class EmergencyPost extends AppCompatActivity {
         direction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
                 String lat=Double.toString(emer_lat);
                 String lon=Double.toString(emer_long);
                 Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
