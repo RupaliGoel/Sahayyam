c0ed9d3ed34387e9f049dce9917822a4c1854d14
_rupaligoel_ <goel.rupali1997@gmail.com>
2018-11-15 14:36:51 +0530

    post view and delete page completed

--- app/src/main/java/com/example/rupali/sos/MyPosts.java
+++ app/src/main/java/com/example/rupali/sos/MyPosts.java
@@ -27,12 +27,6 @@ public class MyPosts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_posts);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
