e706d0aa230be362b45819de98c65d1c265e90b4
Ananta Singh <singhananta@gmail.com>
2018-11-19 16:26:12 +0530

    progress overlay manage appeal

--- app/src/main/java/com/example/rupali/sos/ManageAppeal.java
+++ app/src/main/java/com/example/rupali/sos/ManageAppeal.java
@@ -85,6 +85,13 @@ public class ManageAppeal extends Fragment {
     public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        progressOverlay=view.findViewById(R.id.progress_overlay);
+        progressOverlay.bringToFront();
+
+        // Show progress overlay (with animation):
+        AndroidUtils.animateView(progressOverlay, View.VISIBLE, 0.4f, 200);
+
+
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         user_email = prefs.getString("user_email","admin");
 
@@ -200,7 +207,7 @@ public class ManageAppeal extends Fragment {
 
             }
             // Hide it (with animation):
-            //AndroidUtils.animateView(progressOverlay, View.GONE, 0, 200);
+            AndroidUtils.animateView(progressOverlay, View.GONE, 0, 200);
 
         }
 
